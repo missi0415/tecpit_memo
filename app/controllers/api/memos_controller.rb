a28e63e097eb2c78d9c252de432e251c81245550
missi0415 <missi0415@gmail.com>
2021-07-06 21:22:26 +0900

    formの追加

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -2,14 +2,15 @@ class Api::MemosController < ApplicationController
   def index
     @memos = Memo.order("created_at DESC")
   end
-def create
-  @memo = Memo.new(memo_params)
-  if @memo.save
-    render :show,status: :created
-  else
-    render json: @memo.errors,status: :unp
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      render :show, status: :created
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
   end
-end
 
 private
   def memo_params
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,34 +1,37 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="memo in memos" :key = "memo.id">
-        {{memo.title }} : {{ memo.description }}
+      <li v-for="memo in memos" :key="memo.id">
+        {{ memo.title }} : {{ memo.description }}
       </li>
-    </ul>    
-
+    </ul>
+    <div>
+      <input v-model="title" placeholder="title">
+      <input v-model="description" placeholder="description">
+      <button @click="addMemo">メモを追加</button>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  data: function () {
+  data() {
     return {
-      memos: "memos"
+      memos: "memos",
+      title: '',
+      description: '',
     }
   },
-  mounted () {
+  mounted() {
     this.setMemo();
   },
   methods: {
     setMemo: function () {
-      axios.get('/api/memos')
-      .then(response => (
-      this.memos = response.data
-      ))
-    }
-  } 
+      axios.get("/api/memos").then((response) => (this.memos = response.data));
+    },
+  },
 }
 </script>
 
